b8a86cfbcdcf3f465a7988361c9cf048dd8fffa2
Kevin Dahmani <kevin.dahmani@gmail.com>
2020-10-10 16:02:26 +0200

    initial commit ....

---
+++ src/containers/reservation/reservaton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export const Reservation = () => {
+
+
+    console.log("Réservation component");
+
+    return (
+        <div class="reservation">
+
+        </div>
+    )
+}
