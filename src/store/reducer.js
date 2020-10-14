ca46437f1f7c2f243f7cbe04dde605582b76eb26
Kevin <kevin.dahmani@gmail.com>
2020-10-14 23:38:07 +0200

    Modifications des constantes

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,27 +1,96 @@
+import PARE_BRISE from '../assets/images/PARE_BRISE.svg';
+import LUNETTE_ARRIERE from '../assets/images/LUNETTE_ARRIERE.svg';
+import VITRE_LATERALE_DROITE from '../assets/images/VITRE_LATERALE_DROITE.svg';
+import VITRE_LATERALE_GAUCHE from '../assets/images/VITRE_LATERALE_GAUCHE.svg';
+import IMPACT_LEGER from '../assets/images/IMPACT_LEGER.svg';
+import IMPACT_IMPORTANT from '../assets/images/IMPACT_IMPORTANT.svg';
+import FISSURE from '../assets/images/FISSURE.svg';
+import VITRE_GAUCHE_PORTE_AVANT from '../assets/images/VITRE_GAUCHE_PORTE_AVANT.svg';
+import VITRE_GAUCHE_PORTE_ARRIERE from '../assets/images/VITRE_GAUCHE_PORTE_ARRIERE.svg';
+import VITRE_GAUCHE_CUSTODE_ARRIERE from '../assets/images/VITRE_GAUCHE_CUSTODE_ARRIERE.svg';
+import VITRE_DROITE_PORTE_AVANT from '../assets/images/VITRE_DROITE_PORTE_AVANT.svg';
+import VITRE_DROITE_PORTE_ARRIERE from '../assets/images/VITRE_DROITE_PORTE_ARRIERE.svg';
+import VITRE_DROITE_CUSTODE_ARRIERE from '../assets/images/VITRE_DROITE_CUSTODE_ARRIERE.svg';
+
 import * as actionTypes from './actions';
 
+
+//TODO il faut déplacer les constantes dans un autre fichier
 export const NATURE_INTERVENTION = {
-    PARE_BRISE: 'PARE_BRISE',
-    LUNETTE_ARRIERE: 'LUNETTE_ARRIERE',
-    VITRE_LATERALE_DROITE: 'VITRE_LATERALE_DROITE',
-    VITRE_LATERALE_GAUCHE: 'VITRE_LATERALE_GAUCHE',
+    PARE_BRISE: {
+        label : 'Pare-brise',
+        code: "PARE_BRISE",
+        img: PARE_BRISE
+    },
+    LUNETTE_ARRIERE:  {
+        label : 'Lunette arrière',
+        code: 'LUNETTE_ARRIERE',
+        img: LUNETTE_ARRIERE
+    },
+    VITRE_LATERALE_DROITE: {
+        label : 'Vitre latérale droite',
+        code : 'VITRE_LATERALE_DROITE',
+        img: VITRE_LATERALE_DROITE
+    },
+    VITRE_LATERALE_GAUCHE:  {
+        label : 'Vitre latéral gauche',
+        code:  'VITRE_LATERALE_GAUCHE',
+        img: LUNETTE_ARRIERE,
+        img: VITRE_LATERALE_GAUCHE
+    }
 };
 
 export const VITRE_GAUCHE = {
-    PORTE_AVANT: 'VITRE_GAUCHE_PORTE_AVANT',
-    PORTE_ARRIERE: 'VITRE_GAUCHE_PORTE_ARRIERE',
-    CUSTODE_ARRIERE: 'VITRE_GAUCHE_CUSTODE_ARRIERE',
+    PORTE_AVANT: {
+        label : 'Pare-brise',
+        code: 'VITRE_GAUCHE_PORTE_AVANT',
+        img:VITRE_GAUCHE_PORTE_AVANT
+    },
+    PORTE_ARRIERE: {
+        label : 'Pare-brise',
+        code: 'VITRE_GAUCHE_PORTE_ARRIERE',
+        img:VITRE_GAUCHE_PORTE_ARRIERE
+    },
+    CUSTODE_ARRIERE: {
+        label : 'Pare-brise',
+        code: 'VITRE_GAUCHE_CUSTODE_ARRIERE',
+        img:VITRE_GAUCHE_CUSTODE_ARRIERE
+    }
 };
 export const VITRE_DROITE = {
-    PORTE_AVANT: 'VITRE_DROITE_PORTE_AVANT',
-    PORTE_ARRIERE: 'VITRE_DROITE_PORTE_ARRIERE',
-    CUSTODE_ARRIERE: 'VITRE_DROITE_CUSTODE_ARRIERE',
+    PORTE_AVANT: {
+        label : 'Pare-brise',
+        code:'VITRE_DROITE_PORTE_AVANT',
+        img:VITRE_DROITE_PORTE_AVANT
+    },
+    PORTE_ARRIERE: {
+        label : 'Pare-brise',
+        code :'VITRE_DROITE_PORTE_ARRIERE',
+        img:VITRE_DROITE_PORTE_ARRIERE
+    },
+    CUSTODE_ARRIERE: {
+        label : 'Pare-brise',
+        code:'VITRE_DROITE_CUSTODE_ARRIERE',
+        img:VITRE_DROITE_CUSTODE_ARRIERE
+    }
 };
 
 export const TYPE_DOMMAGE = {
-    IMPACT_LEGER: 'IMPACT_LEGER',
-    IMPACT_IMPORTANT: 'IMPACT_IMPORTANT',
-    FISSURE: 'FISSURE',
+    IMPACT_LEGER: {
+        label : 'Pare-brise',
+        code:'IMPACT_LEGER',
+        img:IMPACT_LEGER
+    },
+    IMPACT_IMPORTANT: {
+        label : 'Pare-brise',
+        code: 'IMPACT_IMPORTANT',
+        img:IMPACT_IMPORTANT
+    },
+    FISSURE: {
+        label : 'Pare-brise',
+        code:'FISSURE',
+        img:FISSURE
+    }
 };
 
 const initialState = {
@@ -31,7 +100,9 @@ const initialState = {
     assureur: '',
     numAssurance: '',
     modele: '',
-    natureIntervention: '',
+    natureIntervention: 'PARE_BRISE',
+    natureInterventionTypeImpact: '',
+    natureInterventionVitre:'',
     nbreImpact: 0,
     zoneVoiture: '',
     nom: '',
