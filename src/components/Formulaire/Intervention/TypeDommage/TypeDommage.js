959782676e7089eefb93bbf170db965c0b144dc8
Kevin <kevin.dahmani@gmail.com>
2020-10-14 23:37:14 +0200

    TypeDommage component

---
+++ src/components/Formulaire/Intervention/TypeDommage/TypeDommage.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { TYPE_DOMMAGE } from '../../../../store/reducer';
+import Card from '../Card/Card';
+import './TypeDommage.css'
+
+const TypeDommage = ({setType, register,natureInterventionTypeImpact}) => {
+    return (
+        <div className="typedommage">
+            <div className="nature-title align-vertically ">
+                <h2 className="title">Type de dommmage</h2>
+                <p className="text-center">
+                    Quelle vitre est endommagée ? 
+                </p>
+            </div>
+
+            <div className="typedommage-content">
+                        <input type="hidden" value={natureInterventionTypeImpact}  name="natureInterventionTypeImpact" ref={register} />
+                        {Object.keys(TYPE_DOMMAGE).map((type , index ) =>{
+                            return (
+                                <div key={type} className="wrapper-card" onClick={()=> setType(TYPE_DOMMAGE[type].code)}>
+                                    <Card 
+                                        img={TYPE_DOMMAGE[type].img} 
+                                        zoom={false}
+                                        active={natureInterventionTypeImpact === TYPE_DOMMAGE[type].code} />
+                                </div> 
+                            )
+                        })
+                    }
+            </div>
+            
+        </div>
+    )
+}
+
+export default TypeDommage
