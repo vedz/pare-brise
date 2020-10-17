47c8344101ede378067d4024559731c566dab1b9
Kevin Dahmani <kevin.dahmani@gmail.com>
2020-10-17 19:42:29 +0200

    nettoyage de la partie reservation pour repartir sur des bases saines :)

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,22 +1,15 @@
 import React from "react"
-import Reservation from './.././../containers/reservation/Reservation'
 import ClientHistory from './.././../containers/reservation/Administration/ClientHistory/ClientHistory'
 import { Switch, Route, useHistory, Redirect } from "react-router-dom";
 
 
-const BodyHapp = React.FunctionComponent= () => {
-    
-     return (              
+const BodyHapp = React.FunctionComponent = () => {
+
+    return (
         <div>
             <Switch>
-                <Route path="/" exact>
-                    <Reservation/>
-                </Route>
-                <Route path="/Reservation">
-                    <Reservation />
-                </Route> 
-                <Route path="/ClientHistory">
-                    <ClientHistory/>
+                <Route path="/">
+                    <ClientHistory />
                 </Route>
             </Switch>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
 
-import reducer from './store/reducer'
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-const store = createStore(reducer, composeEnhancers);
-
-console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
